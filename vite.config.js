0766936cbbc3dcd5e6b6ede2b2fcca0fd1c98426
WatCaL <michal.cieslak03@gmail.com>
2026-07-14 15:24:36 +0200

    Poprawka 1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // UWAGA: Ta ścieżka MUSI być identyczna z nazwą Twojego repozytorium na GitHubie!
   // Założyliśmy, że repozytorium będzie się nazywać "wopr-frontend"
-  base: 'https://github.com/WatCaL/ratweb.git' 
+  base: '/ratweb/'
 })
