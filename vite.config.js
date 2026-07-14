ca047567d76ecccef451c898fb74ac6d1db2c19f
WatCaL <michal.cieslak03@gmail.com>
2026-07-14 15:18:59 +0200

    pierwszy wpis

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // UWAGA: Ta ścieżka MUSI być identyczna z nazwą Twojego repozytorium na GitHubie!
+  // Założyliśmy, że repozytorium będzie się nazywać "wopr-frontend"
+  base: 'https://github.com/WatCaL/ratweb.git' 
+})
